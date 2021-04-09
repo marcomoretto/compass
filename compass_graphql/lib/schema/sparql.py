0688986daef9a3030e9e9b0ebb603cc78e0a7e5a
marcomoretto <marco.moretto@fmach.it>
2021-04-09 11:48:51 +0200

    docs

--- compass_graphql/lib/schema/sparql.py
+++ compass_graphql/lib/schema/sparql.py
@@ -14,6 +14,13 @@ from command.lib.db.compendium.bio_feature import BioFeature
 from compass_graphql.lib.schema.sample import SampleType
 from compass_graphql.lib.utils.compendium_config import CompendiumConfig
 
+from command.lib.db.compendium.sample import Sample
+from django.db.models import Q
+from command.lib.db.compendium.normalization_experiment import NormalizationExperiment
+from command.lib.db.compendium.normalization import Normalization
+
+from command.lib.db.compendium.normalization_design_group import NormalizationDesignGroup
+
 import json
 
 
@@ -45,15 +52,23 @@ class Query(object):
         )
         result = []
 
+        n = kwargs.get('normalization', db['default_normalization'])
+        normalization = Normalization.objects.using(db['name']).get(name=n)
+        smp_ids = set()
+        for exp in NormalizationExperiment.objects.using(db['name']).filter(Q(use_experiment=True) &
+                                                                            Q(normalization=normalization)):
+            smp_ids.update(exp.experiment.sample_set.all().values_list('id', flat=True))
+
         g_path = db['sample_annotation_path'] if kwargs['target'] == 'sample' else db['biofeature_annotation_path']
         g = Graph('Sleepycat', identifier=kwargs['target'])
         g.open(g_path, create=False)
+        valid_samples = Sample.objects.using(db['name']).filter(id__in=smp_ids)
         for x in g.query(kwargs['query']):
             triple = []
             for t in x:
                 if type(t) == Literal and t.datatype == RDF.ID:
                     if kwargs['target'] == 'sample':
-                        s = Sample.objects.using(db['name']).filter(id=str(t)).first()
+                        s = valid_samples.filter(id=str(t)).first()
                     else:
                         s = BioFeature.objects.using(db['name']).filter(id=str(t)).first()
                     if s and kwargs['target'] == 'sample':
@@ -64,6 +79,7 @@ class Query(object):
                         triple.append(None)
                 else:
                     triple.append(t)
-            result.append(triple)
+            if sum([bool(x) for x in triple]):
+                result.append(triple)
         g.close()
         return result
